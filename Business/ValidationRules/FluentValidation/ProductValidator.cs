ee5a4ed711582ef092c224edfee83a89bd2224c8
berkayyuysal <berkayyuysal@gmail.com>
2022-05-15 19:05:35 +0300

    12. ders sonu

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(2);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10)
+                                     .When(p => p.CategoryId == 1);
+
+            RuleFor(p => p.ProductName).Must(StartWithA)                               // kurallar elle yazılabilir
+                                       .WithMessage(" Ürünler A harfi ile başlamalı"); // hataya ait mesajlar elle belirtilebilir
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
